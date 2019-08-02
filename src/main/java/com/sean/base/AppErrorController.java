e6e6597fa31b3b29793795c6e98d9deda8da7dbb
殷海翔 <yin.hx@burgeon.cn>
2019-08-02 20:17:05 +0800

    增加异常处理

---
+++ src/main/java/com/sean/base/AppErrorController.java
@@ -0,0 +1,63 @@
+package com.sean.base;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * web错误 全局配置
+ */
+@Controller
+public class AppErrorController implements ErrorController {
+    private static final String ERROR_PATH = "/error";
+
+    private ErrorAttributes errorAttributes;
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    @Autowired
+    public AppErrorController(ErrorAttributes errorAttributes) {
+
+        this.errorAttributes = errorAttributes;
+    }
+
+    /**
+     * Web页面错误处理
+     */
+    @RequestMapping(value = ERROR_PATH, produces = "text/html")
+    public String errorPageHandler(HttpServletRequest request, HttpServletResponse response) {
+        int status = response.getStatus();
+        switch (status) {
+            case 403:
+                return "403";
+            case 404:
+                return "404";
+            case 500:
+                return "500";
+        }
+
+        return "index";
+    }
+
+
+    private int getStatus(HttpServletRequest request) {
+        Integer status = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (status != null) {
+            return status;
+        }
+
+        return 500;
+    }
+}
