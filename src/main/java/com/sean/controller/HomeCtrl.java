9c3ef075624c72ce9e5a0c77464b5874c5945670
殷海翔 <yin.hx@burgeon.cn>
2019-08-02 20:05:35 +0800

    增加ApiResponse

--- src/main/java/com/sean/controller/HomeCtrl.java
+++ src/main/java/com/sean/controller/HomeCtrl.java
@@ -1,8 +1,10 @@
 package com.sean.controller;
 
+import com.sean.base.ApiResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeCtrl {
@@ -10,7 +12,15 @@ public class HomeCtrl {
     @GetMapping(value = {"/", "/index"})
     public String index(Model model) {
         model.addAttribute("name", "sean");
-        return "index";
+       return "index";
+    }
+
+
+
+    @GetMapping("/get")
+    @ResponseBody
+    public ApiResponse get(Model model) {
+        return ApiResponse.ofMessage(200, "成功");
     }
 
 
