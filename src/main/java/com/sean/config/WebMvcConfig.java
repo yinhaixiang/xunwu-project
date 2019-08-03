f479eb7f5aec612083c76d831eba52d9050c0afa
殷海翔 <yin.hx@burgeon.cn>
2019-08-03 14:12:52 +0800

    修改pom

--- src/main/java/com/sean/config/WebMvcConfig.java
+++ src/main/java/com/sean/config/WebMvcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -58,6 +59,9 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter implements Application
         // 支持Spring EL表达式
         templateEngine.setEnableSpringELCompiler(true);
 
+        // 支持SpringSecurity方言
+        SpringSecurityDialect securityDialect = new SpringSecurityDialect();
+        templateEngine.addDialect(securityDialect);
         return templateEngine;
     }
 
