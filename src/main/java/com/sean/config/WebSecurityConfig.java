f3c5f006298c5f19481b700904d46bafb64ba085
殷海翔 <yin.hx@burgeon.cn>
2019-08-03 14:15:25 +0800

    修改WebSecurityConfig

--- src/main/java/com/sean/config/WebSecurityConfig.java
+++ src/main/java/com/sean/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
