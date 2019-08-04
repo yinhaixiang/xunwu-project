80ff33885681e6acd24520572227713a8842eb0a
殷海翔 <yin.hx@burgeon.cn>
2019-08-04 20:24:41 +0800

    修改AdminController

--- src/main/java/com/sean/controller/AdminController.java
+++ src/main/java/com/sean/controller/AdminController.java
@@ -1,7 +1,16 @@
 package com.sean.controller;
 
+import com.sean.base.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by 瓦力.
@@ -11,6 +20,7 @@ public class AdminController {
 
     /**
      * 后台管理中心
+     *
      * @return
      */
     @GetMapping("/admin/center")
@@ -20,6 +30,7 @@ public class AdminController {
 
     /**
      * 欢迎页
+     *
      * @return
      */
     @GetMapping("/admin/welcome")
@@ -29,6 +40,7 @@ public class AdminController {
 
     /**
      * 管理员登录页
+     *
      * @return
      */
     @GetMapping("/admin/login")
@@ -38,6 +50,7 @@ public class AdminController {
 
     /**
      * 房源列表页
+     *
      * @return
      */
     @GetMapping("admin/house/list")
@@ -48,11 +61,36 @@ public class AdminController {
 
     /**
      * 新增房源功能页
+     *
      * @return
      */
     @GetMapping("admin/add/house")
     public String addHousePage() {
         return "admin/house-add";
+    }
+
+
+    /**
+     * 上传图片接口
+     *
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file") MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+
+        String fileName = file.getOriginalFilename();
+
+        InputStream inputStream = file.getInputStream();
+
+        System.out.println(inputStream);
+
+        return ApiResponse.ofSuccess(null);
+
     }
 
 
