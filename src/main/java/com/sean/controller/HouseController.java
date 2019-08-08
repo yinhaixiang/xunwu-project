e052e067105792fcc60fac5491c1d9df43fde559
殷海翔 <yin.hx@burgeon.cn>
2019-08-08 14:21:20 +0800

    完成show

--- src/main/java/com/sean/controller/HouseController.java
+++ src/main/java/com/sean/controller/HouseController.java
@@ -8,6 +8,7 @@ import com.sean.dto.HouseDTO;
 import com.sean.entity.Subway;
 import com.sean.entity.SubwayStation;
 import com.sean.entity.SupportAddress;
+import com.sean.entity.User;
 import com.sean.form.RentSearch;
 import com.sean.service.IAddressService;
 import com.sean.service.IHouseService;
@@ -15,14 +16,12 @@ import com.sean.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HouseController {
@@ -154,5 +153,35 @@ public class HouseController {
         return "rent-list";
     }
 
+
+    @GetMapping("rent/house/show/{id}")
+    public String show(@PathVariable(value = "id") Long houseId, Model model) {
+        if (houseId <= 0) {
+            return "404";
+        }
+
+        ServiceResult<HouseDTO> serviceResult = houseService.findCompleteOne(houseId);
+        if (!serviceResult.isSuccess()) {
+            return "404";
+        }
+
+        HouseDTO houseDTO = serviceResult.getResult();
+        Map<SupportAddress.Level, SupportAddress>
+                addressMap = addressService.findCityAndRegion(houseDTO.getCityEnName(), houseDTO.getRegionEnName());
+
+        SupportAddress city = addressMap.get(SupportAddress.Level.CITY);
+        SupportAddress region = addressMap.get(SupportAddress.Level.REGION);
+
+        model.addAttribute("city", city);
+        model.addAttribute("region", region);
+
+        User user = userService.getById(houseDTO.getAdminId());
+        model.addAttribute("agent", user);
+        model.addAttribute("house", houseDTO);
+
+        model.addAttribute("houseCountInDistrict", 0);
+
+        return "house-detail";
+    }
 
 }
