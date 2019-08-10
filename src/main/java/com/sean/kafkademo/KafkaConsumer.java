228dcbee73b4cbac5a62af7111dcf8b3a37c5972
殷海翔 <yin.hx@burgeon.cn>
2019-08-10 14:28:04 +0800

    完成kafkademo

---
+++ src/main/java/com/sean/kafkademo/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package com.sean.kafkademo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class KafkaConsumer {
+
+
+    @KafkaListener(topics = {"sean"})
+    public void receive(String message) {
+        log.info("++++++++++++++++KafkaMessageConsumer 接收到消息：" + message);
+    }
+}
